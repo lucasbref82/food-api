098a59db5a6d1c70a37ec79429968dc8b1ad90d0
Lucas Ferreira <lucas.ferreira@digicade.com.br>
2023-04-23 04:00:33 -0300

    Adicionando response status

--- src/main/java/br/com/foodapi/controller/CozinhaController.java
+++ src/main/java/br/com/foodapi/controller/CozinhaController.java
@@ -3,10 +3,12 @@ package br.com.foodapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.foodapi.domain.entity.Cozinha;
@@ -21,17 +23,20 @@ public class CozinhaController {
 	private CozinhaService cozinhaService;
 	
 	@GetMapping
+	@ResponseStatus(code = HttpStatus.OK)
 	public List<Cozinha> findAll(){
 		return cozinhaService.findAll();
 	}
 	
 	// Customizando representação XML
 	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+	@ResponseStatus(code = HttpStatus.OK)
 	public CozinhasXmlWrapper findAllXml(){
 		return new CozinhasXmlWrapper(cozinhaService.findAll());
 	}
 	
 	@GetMapping("/{id}")
+	@ResponseStatus(code = HttpStatus.OK)
 	public Cozinha findById(@PathVariable Long id) {
 		return cozinhaService.findById(id);
 	}
--- src/main/java/br/com/foodapi/controller/EstadoController.java
+++ src/main/java/br/com/foodapi/controller/EstadoController.java
@@ -3,10 +3,12 @@ package br.com.foodapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.foodapi.domain.entity.Estado;
@@ -20,11 +22,13 @@ public class EstadoController {
 	private EstadoService estadoService;
 	
 	@GetMapping("/{id}")
+	@ResponseStatus(code = HttpStatus.OK)
 	public Estado findById(@PathVariable Long id) {
 		return estadoService.findById(id);
 	}
 	
 	@GetMapping
+	@ResponseStatus(code = HttpStatus.OK)
 	public List<Estado> findAll(){
 		return estadoService.findAll();
 	}
